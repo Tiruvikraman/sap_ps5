b3c92b6b7635d2becb98e5157100b7b0e7980669
VarshaKumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2024-04-20 19:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from japan import get_japan_price
 from singapore import get_singapore_price
 from uk import get_uk_price
 from aust import get_aust_price
+from usa import get_usa_price
 
 product_name = "iphone 15 pro"
 
@@ -23,4 +24,7 @@ price_info = get_uk_price(product_name)
 print(price_info)
 
 price_info = get_aust_price(product_name)
+print(price_info)
+
+price_info = get_usa_price(product_name)
 print(price_info)

ffa2d1ba8fcd53a1419a94b830a92e4a80f74725
VarshaKumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2024-04-20 22:37:59 +0530

    Update and rename singapore.py to price_extraction/singapore.py

--- price_extraction/singapore.py
+++ price_extraction/singapore.py
@@ -6,7 +6,10 @@ from bs4 import BeautifulSoup
 
 
 def get_singapore_price(product_name):
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(options=options)
+    
     product_name = product_name.replace(' ', '+')
     url = f'https://amazon.sg/s?k={product_name}'
 
@@ -38,7 +41,6 @@ def get_singapore_price(product_name):
     soup = BeautifulSoup(html_after_apply, 'html.parser')
 
     updated_url = driver.current_url
-    print(updated_url)
     url = updated_url
 
     driver.get(url)

ab519029bf7cd4f6ef77d9f470a6e83162c784e8
VarshaKumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2024-04-20 11:19:29 +0530

    Update and rename amazon_singapore.py to singapore.py

---
+++ singapore.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+
+def get_singapore_price(product_name):
+    driver = webdriver.Chrome()
+    product_name = product_name.replace(' ', '+')
+    url = f'https://amazon.sg/s?k={product_name}'
+
+    driver.get(url)
+
+    deliver_to_link = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "nav-global-location-popover-link"))
+    )
+    deliver_to_link.click()
+
+    postcode_input1 = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "GLUXZipUpdateInput"))
+    )
+
+    postcode_input1.clear()
+    postcode_input1.send_keys("238858")
+
+    apply_button = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "GLUXZipUpdate"))
+    )
+    apply_button.click()
+
+    print("Postcode applied successfully.")
+
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.a-price-whole")))
+
+    html_after_apply = driver.page_source
+
+    soup = BeautifulSoup(html_after_apply, 'html.parser')
+
+    updated_url = driver.current_url
+    print(updated_url)
+    url = updated_url
+
+    driver.get(url)
+
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'html.parser')
+    symbol = soup.find('span', class_='a-price-symbol').get_text()
+    price_text = soup.find('span', class_='a-price-whole').get_text().strip(',').replace('\u202f', '')
+
+    price_text = price_text.replace(',', '').replace('.', '')
+
+    price = int(price_text)
+
+    return [symbol, price]

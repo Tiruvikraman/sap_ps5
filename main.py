150a2cde279968a578016d2c0d8dde60680f9c6a
VarshaKumaraguru <122372761+VarshaKumaraguru@users.noreply.github.com>
2024-04-20 11:28:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from france import get_france_price
+from germany import get_germany_price
+from japan import get_japan_price
+from singapore import get_singapore_price
+from uk import get_uk_price
+from aust import get_aust_price
+
+product_name = "iphone 15 pro"
+
+price_info = get_france_price(product_name)
+print(price_info)
+
+price_info = get_germany_price(product_name)
+print(price_info)
+
+price_info = get_japan_price(product_name)
+print(price_info)
+
+price_info = get_singapore_price(product_name)
+print(price_info)
+
+price_info = get_uk_price(product_name)
+print(price_info)
+
+price_info = get_aust_price(product_name)
+print(price_info)
